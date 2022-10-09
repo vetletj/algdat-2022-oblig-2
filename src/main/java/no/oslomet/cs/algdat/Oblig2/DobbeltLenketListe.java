c5a3887aef7883c729e56c576caa69f9742e5f81
vetletj <67962681+vetletj@users.noreply.github.com>
2022-10-09 21:20:05 +0200

    forsøk utkast oppgave 8 b

--- src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig2/DobbeltLenketListe.java
@@ -223,7 +223,8 @@ public class DobbeltLenketListe<T> implements Liste<T> {
 
     @Override
     public Iterator<T> iterator() {
-        throw new UnsupportedOperationException();
+        return (Iterator<T>) new DobbeltLenketListeIterator(); // Usikker på om dette er riktig
+        //throw new UnsupportedOperationException();
     }
 
     public Iterator<T> iterator(int indeks) {
